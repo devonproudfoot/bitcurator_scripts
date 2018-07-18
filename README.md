d8e5e4465de30d0b57d88530c9529834f350ee5f
Devon Proudfoot <proudfoot.devon@gmail.com>
2018-07-18 15:38:40 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bitcurator_scripts
+
+Run create_accessions.sh to automatically create folders for the media that is being transferred.
+  TO-DO - Move photograph of media, create logs
---
+++ create_accession.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+accession=$(zenity --entry --title="Transfer Information" --text="What is the accession number?")
+
+if [ ! -d "/home/bcadmin/Desktop/pending_transfers/$accession" ]; then
+  mkdir /home/bcadmin/Desktop/pending_transfers/$accession
+fi
+
+unique_id=$(zenity --entry --title="Transfer Information" --text="What is the unique identifier?")
+
+if [ -d "/home/bcadmin/Desktop/pending_transfers/$accession/$unique_id" ]; then
+  zenity --info --title="Error" text="Folder already exists!"
+  exit
+else
+  mkdir /home/bcadmin/Desktop/pending_transfers/$accession/$unique_id
+fi
+
+mkdir /home/bcadmin/Desktop/pending_transfers/$accession/$unique_id/metadata
+
+zenity --info --title="Folder Created" text="The folders has been created for $accession/$unique_id"
