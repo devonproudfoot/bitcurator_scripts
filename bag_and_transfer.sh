3cfcca3bef1f37532649ec41cefcc979d0f49093
Devon Proudfoot <proudfoot.devon@gmail.com>
2018-07-23 16:52:18 -0500

    Added end text

--- bag_and_transfer.sh
+++ bag_and_transfer.sh
@@ -5,3 +5,5 @@ accession=$(zenity --entry --title="Bag and Transfer" --text="Please enter the a
 python /usr/local/bin/bagit.py /home/bcadmin/Desktop/pending_transfers/$accession
 
 rsync -r -t -p -o -g -v --progress - c -D -s /home/bcadmin/Desktop/pending_transfers/$accession /media/sf_BC_share/transfers
+
+zenity --info --title="Transfer Complete" --text="The transfer was successfull \n\n Please find the material in the 'Transfers' folder."
