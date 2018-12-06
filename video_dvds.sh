e33214769e59a7d267b100d13192e583f37e69f5
Devon Proudfoot <proudfoot.devon@gmail.com>
2018-12-06 15:14:10 -0600

    added args to ddrescue

--- video_dvds.sh
+++ video_dvds.sh
@@ -12,7 +12,7 @@ unique_folder=${accession}_${unique_id}
 
 disk_md5=$(md5sum /dev/sr1 | cut -d" " -f1)
 
-ddrescue /dev/sr1 /home/bcadmin/Desktop/pending_transfers/$accession/$unique_folder/$unique_folder.iso
+ddrescue -b 2048 -v /dev/sr1 /home/bcadmin/Desktop/pending_transfers/$accession/$unique_folder/$unique_folder.iso /home/bcadmin/Desktop/pending_transfers/$accession/$unique_folder/$unique_folder.log
 
 iso_md5=$(md5sum /home/bcadmin/Desktop/pending_transfers/$accession/$unique_folder/$unique_folder.iso | cut -d" " -f1)
 
@@ -20,11 +20,3 @@ echo -n "Disk MD5: "
 echo $disk_md5
 echo -n "ISO MD5: "
 echo $iso_md5
-
-if ["$disk_md5" -eq "$iso_md5" ]; then
-	echo -n "Image is valid!"
-	echo -n "Creating MP4 file, please wait"
-	HandBrakeCLI -i /home/bcadmin/Desktop/pending_transfers/$accession/$unique_folder/$unique_folder.iso -o /home/bcadmin/Desktop/pending_transfers/$accession/$unique_folder/$unique_folder.mp4
-else
-	echo - m "ERROR: Image is NOT valid!"
-fi
