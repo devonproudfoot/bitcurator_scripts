68040e31c0feb0ca3d778ef8c931fca5733553ca
Devon Proudfoot <proudfoot.devon@gmail.com>
2018-12-18 15:49:09 -0600

    prepare for ingest sh

---
+++ prepare_for_ingest.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo -n "Please enter the accession number: "
+read accession_number
+
+parent_directory=/home/bcadmin/Desktop/pending_transfers/$accession_number
+
+new_location=/media/sf_backlog/$accession_number
+
+cd /media/sf_backlog/
+mkdir $accession_number
+
+cd $parent_directory
+
+for folder in $parent_directory/*
+do
+	echo $folder
+	bagit.py $folder
+	rsync -avc $folder $new_location
+	new_folder=$(basename $folder)
+	cd $new_location
+	zip -r $new_location/$new_folder.zip *
+	chmod 755 $new_location/$new_folder.zip
+	rm -r $new_location/$new_folder
+done
