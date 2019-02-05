0dda778ba50a7d115ce191c6093182cbe12b4820
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-02-05 17:54:31 -0500

    new items

--- cli_install.sh
+++ cli_install.sh
@@ -9,5 +9,11 @@ sudo pip3 install hashlib
 echo -n "Installing pdf2html"
 sudo apt-get install wkhtmltopdf
 
-echo -n "Installing handbrake CLI"
-sudo apt-get install handbrake-cli handbrake-gtk
+echo -n "Installing handbrake CLI and GUI"
+sudo apt-get install handbrake-cli
+sudo apt-get install handbrake-gtk
+
+mkdir /home/bcadmin/Desktop/pending_transfers
+mkdir /home/bcadmin/Desktop/photographs
+mkdir /home/bcadmin/Desktop/photographs/transferred
+mkdir /home/bcadmin/Desktop/bc_reports
