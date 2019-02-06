69c8688894c616aec826debf0cb82f5898373399
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-02-06 17:46:49 -0500

    Update

--- arrangement.sh
+++ arrangement.sh
@@ -12,15 +12,16 @@ while true; do
 	fi
 	echo -n "Please enter the series and item name (ex. 'Photographs--Basketball'): "
 	read SIP
-	mkdir /Users/proudfootd/Desktop/born-digital_working/$coll_ID--$SIP /Users/proudfootd/Desktop/born-digital_working/$coll_ID--$SIP/objects
-	#mkdir /home/bcadmin/Desktop/working/$coll_ID--$SIP /home/bcadmin/Desktop/working/$coll_ID--$SIP/objects
-    touch /Users/proudfootd/Desktop/born-digital_working/$coll_ID--$SIP.txt
-	#touch /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
-    open /Users/proudfootd/Desktop/born-digital_working/$coll_ID--$SIP.txt
-    #open /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
+	mkdir /home/bcadmin/Desktop/working/$coll_ID--$SIP
+    touch /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
+    open /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
+	xdg-open /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
     read -p "The text file has been opened, please copy the paths to the records to be included in the SIP.  Each folder or file should be be on a new line.  Hit enter when the file has been saved."
     while IFS='' read -r line || [[ -n "$line" ]]; do
         echo $line
-        rsync -avc --progress "$line" /Users/proudfootd/Desktop/born-digital_working/$coll_ID--$SIP/objects
-    done < "/Users/proudfootd/Desktop/born-digital_working/$coll_ID--$SIP.txt"
+        rsync -avc --progress "$line" /home/bcadmin/Desktop/working/$coll_ID--$SIP
+    done < "/home/bcadmin/Desktop/working/$coll_ID--$SIP.txt"
+	rm /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
+	bagit.py --md5 /home/bcadmin/Desktop/working/$coll_ID--$SIP
+	chmod -R 755 /home/bcadmin/Desktop/working/$coll_ID--$SIP
 done
