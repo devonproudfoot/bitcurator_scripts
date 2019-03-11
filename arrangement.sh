5e8130c2f2242a3289563025bf97c352ab69ad1d
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-03-11 09:49:15 -0500

    removed bag lines

--- arrangement.sh
+++ arrangement.sh
@@ -13,15 +13,17 @@ while true; do
 	echo -n "Please enter the series and item name (ex. 'Photographs--Basketball'): "
 	read SIP
 	mkdir /home/bcadmin/Desktop/working/$coll_ID--$SIP
+	mkdir /home/bcadmin/Desktop/working/$coll_ID--$SIP/objects
+	mkdir /home/bcadmin/Desktop/working/$coll_ID--$SIP/metadata
     touch /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
     open /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
 	xdg-open /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
     read -p "The text file has been opened, please copy the paths to the records to be included in the SIP.  Each folder or file should be be on a new line.  Hit enter when the file has been saved."
     while IFS='' read -r line || [[ -n "$line" ]]; do
         echo $line
-        rsync -t -O --protect-args -vv --chmod=Fug+rw,o-rwx,Dug+rwx,o-rwx -r "$line" /home/bcadmin/Desktop/working/$coll_ID--$SIP
+        rsync -t -O --protect-args -vv --chmod=Fug+rw,o-rwx,Dug+rwx,o-rwx -r "$line" /home/bcadmin/Desktop/working/$coll_ID--$SIP/objects
     done < "/home/bcadmin/Desktop/working/$coll_ID--$SIP.txt"
 	rm /home/bcadmin/Desktop/working/$coll_ID--$SIP.txt
-	bagit.py --md5 /home/bcadmin/Desktop/working/$coll_ID--$SIP
-	chmod -R 755 /home/bcadmin/Desktop/working/$coll_ID--$SIP
+	#bagit.py --md5 /home/bcadmin/Desktop/working/$coll_ID--$SIP
+	#chmod -R 755 /home/bcadmin/Desktop/working/$coll_ID--$SIP
 done
