a3cb7dc17fde6850bb906ccec88714c887adc2a7
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-07-26 15:44:23 -0500

    Set up to handle more cases

--- msg_to_mbox.sh
+++ msg_to_mbox.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-msgconvert --mbox shd.mbox SHD_emails/*.msg
+read -n "What is the folder you would like to convert? "
+read input
+
+read -n "What would you like to name the mbox file? "
+read output
+
+
+msgconvert --mbox $output $input/*msg
